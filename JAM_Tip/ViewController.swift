47d096883258b3bb5479bd185f05ce7c323ad8ba
MoralesJohn <beljam@gmail.com>
2017-02-17 11:08:31 -0800

    Initial Commit

---
+++ JAM_Tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JAM_Tip
+//
+//  Created by Jack on 17 Feb 17.
+//  Copyright © 2017 John Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
